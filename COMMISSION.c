70f0991fe6d05c261bd32f3ac73dea4702094917
IsmatChebet <chebetismat@gmail.com>
2025-06-16 16:06:24 +0300

    Branch2: 2nd Commit

--- COMMISSION.c
+++ COMMISSION.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+
+double commissioncalculation() {
+    double salesVolume;
+    double commissionRate;
+    double commission;
+
+
+    printf("Enter Sales Volume : ");
+    scanf("%lf", &salesVolume);
+
+
+    printf("Enter Commission Rate : ");
+    scanf("%lf", &commissionRate);
+
+
+    commission = commissionRate * salesVolume;
+
+    printf("Commission = %.2lf\n", commission);
+    return 0;
+}
+int main() {
+
+    commissioncalculation();
+
+    return 0;
+}
