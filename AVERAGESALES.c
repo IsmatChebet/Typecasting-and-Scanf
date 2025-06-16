bcdb1df801bc8a169a56e50f3ccaafefe0236ae2
IsmatChebet <chebetismat@gmail.com>
2025-06-16 15:18:03 +0300

    Branch1: 2nd Commit

--- AVERAGESALES.c
+++ AVERAGESALES.c
@@ -2,4 +2,18 @@
 #include <stdlib.h>
 int main(){
 
+    int total_sales, days;
+
+    printf("Enter total sales (UGX): ");
+    scanf("%d", &total_sales);
+
+    printf("Enter number of days : ");
+    scanf("%d", &days);
+
+    float average_sales = (float)total_sales / days;
+    printf("Average sales per day : UGX %.2f\n", average_sales);
+
+    return 0;
 }
+
+
